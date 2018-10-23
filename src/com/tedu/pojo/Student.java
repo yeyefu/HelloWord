9369d734efe4321f7143ba31844476d51b56c10b
yeyefu <718821703@qq.com>
2018-10-23 19:26:06 +0800

    第一次提交/菜单完成

---
+++ src/com/tedu/pojo/Student.java
@@ -0,0 +1,31 @@
+package com.tedu.pojo;
+
+public class Student {
+    private  int id;
+    private  String name;
+    private  String sex;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
